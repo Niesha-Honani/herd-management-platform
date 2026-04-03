a2b320e0cd80e2ef9aed2ded785140d10942985a
VaNiesha Honani <vhonani77@gmail.com>
2026-04-03 00:25:02 -0700

    login is public and dashboard and herds are behind protected routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from '../pages/LoginPage'
 import DashboardPage from "../pages/DashboardPage";
 import HerdsPage from "../pages/HerdsPage";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
 function AppRoutes() {
     return (
@@ -9,8 +10,18 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/herds" element={<HerdsPage />} />
+                <Route path="/dashboard" element={
+                    <ProtectedRoute>
+                        <DashboardPage />
+                    </ProtectedRoute>
+                    }
+                />
+                <Route path="/herds" element={
+                    <ProtectedRoute>
+                        <HerdsPage />
+                    </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     )
