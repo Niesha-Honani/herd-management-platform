be8e25fb4d788839af4cf6ad283e42f5112a0e5c
VaNiesha Honani <vhonani77@gmail.com>
2026-04-11 12:41:19 -0700

    migration fix

--- run_compose.sh
+++ run_compose.sh
@@ -4,8 +4,6 @@ set -e
 docker compose up -d --build
 # make sure the postgres container is ready, then run migrations
 sleep 5
+docker compose exec web python manage.py makemigrations accounts
 docker compose exec web python manage.py makemigrations 
 docker compose exec web python manage.py migrate
-docker compose exec web python manage.py createsuperuser
-docker compose ps
-docker compose start
